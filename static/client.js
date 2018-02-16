000d9ac017b53c21d6db5319609f21886309774c
Ania <ania.makes@gmail.com>
2018-02-16 17:18:50 +0000

    client console logs the correct selection

--- static/client.js
+++ static/client.js
@@ -1,16 +1,32 @@
-var placeOrderButton = document.getElementById("place-order");
-
+let placeOrderButton = document.getElementById("place-order");
 placeOrderButton.addEventListener("click", moveToOrderPage);
+
+let proceedToSummary = document.getElementById("proceedToSummary")
 
 function moveToOrderPage(event){
   event.preventDefault();
-
-// create a drop down to start with, keep hidden by default, and reveal on button click
 
   const quantityPicker = document.getElementsByClassName("quantity-picker");
 
   Array.from(quantityPicker).forEach(item => item.style.visibility = "visible");
 
   placeOrderButton.style.visibility = "hidden";
+  proceedToSummary.style.visibility = "visible";
+}
 
+document.getElementById("orderForm").addEventListener("submit", tallyUp);
+
+function tallyUp(event){
+  event.preventDefault();
+  let incomingSelection = Array.from(event.path[0]);
+
+  let order = [];
+
+  incomingSelection.forEach(function(selectOption){
+    if (selectOption.value.split("-")[1] > 0){
+      order.push(selectOption.value);
+    }
+  })
+
+  console.log(order);
 }
